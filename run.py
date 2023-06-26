3195ce287bced66fbcbbe965af32b2e31359a3a6
codewithmaik <>
2023-06-26 19:28:28 +0000

    Add comments to classes and its methods

--- run.py
+++ run.py
@@ -1,24 +1,31 @@
 from datetime import datetime
 
 class Task():
+    """ Class to create instance of a task """
     def __init__(self, task_title, task_description, task_date):
         self.task_title = task_title
         self.task_description = task_description
         self.task_date = task_date
     
-
-
-
 class TaskManager():
+    """ Class to create instance of the task manager """ 
     def __init__(self):
+        """ List to store the created task objects """
         self.tasks = []
 
     def create_task():
+        """
+        - Gets user input
+        - Checks if task_date input is a valid date
+        - Creates task and appends to list
+        - Prints out message to user
+        """
         task_title = str(input("Enter a task title: "))
         task_description = str(input("Describe the new task: "))
         task_date = str(input("When must the task be finished (YYYY-MM-DD): "))
 
         try:
+            """ Converts string into datetime object and checks if it is valid """
             task_date = datetime.strptime(task_date, "%d-%m-%Y").date()
         except ValueError:
             print("Invalid date format. Task creation failed.")
@@ -28,6 +35,7 @@ class TaskManager():
         print("Task created succesfully.")
     
     def main():
+        """ Main method to run the program """
         while True:
             print("Welcome to DAILY - your task manager.")
 
