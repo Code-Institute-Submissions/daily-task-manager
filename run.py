d6d9e6cd5d4f84bf3f9b59aa4b7f4a469c0bc37f
codewithmaik <>
2023-06-26 18:42:05 +0000

    Create task manager class, add tasks list in constructor and add main function to class

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+
 class Task():
     def __init__(self, task_title, task_description, task_date):
         self.task_title = task_title
         self.task_description = task_description
         self.task_date = task_date
 
-def main():
-    tasks = []
-    while True:
+
+class TaskManager():
+    def __init__(self):
+        self.tasks = []
+    
+    def main():
+        while True:
         print("Welcome to DAILY - your task manager.")
